871ee5fbfb9af4237ccdbbb70344cad478a9c919
Remy Duijsens <remyduijsens@gmail.com>
2023-05-23 23:53:49 +0200

    add version lib and version page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, jsonify, render_template, request
+from remla23_team17_lib.lib.version_util import VersionUtil
 import requests
 
 app = Flask(__name__)
@@ -12,6 +13,12 @@ EXPOSE_PORT = os.environ.get("EXPOSE_PORT", 80)
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+
+@app.route("/version", methods=["GET"])
+def version():
+    operation_version_util = VersionUtil("remla23-team17/operation")
+    return f"{operation_version_util.get_version()}"
 
 
 @app.route("/sendreview", methods=["POST"])
